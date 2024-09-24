be604abd84a8019972629ff7f9c27c3101afdb4f
devrakibulrahman <devrakibulrahman@gmail.com>
2024-09-24 17:00:17 +0600

    main content added with responsive on 9/24/2024 at 05:00 pm

--- js/index.js
+++ js/index.js
@@ -1,20 +1,4 @@
 // responsive navbar
-function addClassList(id){
-    const addClassList = id;
-    addClassList.classList.add(
-        "right-[-120%]"
-    );
-
-    return;
-};
-
-function removeClassList(id){
-    const removeClassList = id;
-    removeClassList.classList.remove(
-        "right-[0%]"
-    );
-}
-
 const openBtn = document.getElementById('openBtn');
 openBtn.addEventListener('click', function(){
     const mobileNav = document.getElementById('mobileNav');
@@ -27,16 +11,28 @@ openBtn.addEventListener('click', function(){
     );
 });
 
+function addOrRemove(addId, removeId){
+    const add = addId;
+    addId.classList.add(
+        "right-[-120%]"
+    );
+
+    const remove = removeId;
+    remove.classList.remove(
+        "right-[0%]"
+    );
+
+    return;
+};
+
 const closeBtn = document.getElementById('closeBtn');
 closeBtn.addEventListener('click', function(){
     const mobileNav = document.getElementById('mobileNav');
-    addClassList(mobileNav);
-    removeClassList(mobileNav);
+    addOrRemove(mobileNav, mobileNav);
 });
 
 const navLink = document.getElementById('navLink');
 navLink.addEventListener('click', function(){
     const mobileNav = document.getElementById('mobileNav');
-    addClassList(mobileNav);
-    removeClassList(mobileNav);
+    addOrRemove(mobileNav, mobileNav);
 });
